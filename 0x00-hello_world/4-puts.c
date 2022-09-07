6c7a4cb1e35688ac99398cfd2ddf4b6983986cc2
filizerom <filimonzerom0@gmail.com>
2022-09-07 02:00:13 -0700

    a C program that prints exactly with function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *int main 
+ *use puts
+ * Return: 0
+ */
+
+  int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
+
