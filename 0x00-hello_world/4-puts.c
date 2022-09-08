481e151208f64fd30d432bd181905b6e50615620
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-08 11:51:06 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- *int main 
- *use puts
- * Return: 0
- */
+*int main 
+*use puts
+* Return: 0
+*/
 
-  int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
