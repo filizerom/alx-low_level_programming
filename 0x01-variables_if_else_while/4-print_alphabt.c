e2ba92abe168e1fd8770664905c6e043f0e59bb6
filizerom <filimonzerom0@gmail.com>
2022-09-11 09:18:06 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+/**
+ * main - print all the lettters except e and q
+ * Description: with main function
+ * return: 0
+ * /
+int main(void)
+{
+char ap;
+for(ap ='a'; ap<='z'; ap++)
+{
+if(ap !='e' && ap !='q')
+{
+       putchar(ap);
+}
+}
+putchar('\n');
+return (o);
+}
