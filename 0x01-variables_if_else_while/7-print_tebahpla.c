a587cbfc5d0f369145c81f21af87503ccf6daaa7
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-12 12:37:09 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,14 @@
  * main - print lower case in reverse
  * Description: use main function and char
  * return 0
+ */
 int main(void)
 {
-char i;
-for (i = 'z'; i >= 'a'; i--)
+	char i;
+	for (i = 'z'; i >= 'a'; i--)
 {
-	putchar(i);
+		putchar(i);
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
