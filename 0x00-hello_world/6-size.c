f7b344b11b4e6b08d646f0d47c279f3fb44705bd
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-12 13:54:49 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main -  a C program that prints the size of various types on the computer it is compiled and run on
+ * main -  a C program that prints the sizeof.
  *descriptiion: use main function
  *return (0)
  */
