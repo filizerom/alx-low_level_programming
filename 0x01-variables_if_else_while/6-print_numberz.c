ead289ffe3b5d8a309d7cd41bcda0ba741c0f115
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-12 11:29:56 -0700

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print all digit number of 10 starting from zero 
+ * main - print all digit number of 10 starting from zero
  * Description: use main function and putchar only
  *Return: 0
  */
 int main(void)
 {
 char i;
-for (i = '0'; i<='9'; i++)
+for (i = '0'; i <= '9'; i++)
 {
 	putchar(i);
 }
 putchar("\n");
-return: 0
+return 0;
 }
