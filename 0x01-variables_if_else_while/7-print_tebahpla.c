8aeaca87e204caddefb9f2880465a74fa2d4205f
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-12 12:19:21 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,8 +9,11 @@
  */
 int main(void)
 {
-char i;
-for (i = 'z'; i >= 'a'; i--)
-{	putchar(i)
-putchar('\n');
-return (0);
+  char i;
+  for (i = 'z'; i >= 'a'; i--)
+  {
+    putchar(i)
+  }
+  putchar('\n');
+  return (0);
+}
