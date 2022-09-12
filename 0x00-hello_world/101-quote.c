77f9fab28ed7b1320a02002a9b59e01f6a7788b0
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-12 14:23:02 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a C program that prints exactly below
+ * main - program that prints exactly below
  * Description: use main function
  * return 0
  */
 int main(void)
 {
 write(2,
-      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-     59);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 
 return (1);
 }
