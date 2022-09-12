39324ab08e17fb1c5fb97ca10c543e888428b347
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-12 14:29:50 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 /**
  * main - program that prints exactly below
  * Description: use main function anf for it 
- * return 0
+ * return 1
  */
 
 int main(void)
 {
-  write(2,
-  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-  59);
-  return (1);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+return (1);
 }
