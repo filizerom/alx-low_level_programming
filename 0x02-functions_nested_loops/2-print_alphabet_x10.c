f99839bf27de6b581503e664c7b175a7d001e250
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-13 09:50:29 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,18 @@
 
 /**
  * main - prints 10 times the alphabet, in lowercase, followed by a new line
- * 
+ * Description: use main function.
  * return: 0
  */
 void print_alphabet_x10(void)
 {
-char i, j;
-for (j = 0; j <= 9; j++)
-
-	for (i = 'a'; i <= 'z'; i++)
-{
-		_putchar(i);
-
-}
-	_putchar('\n');
-}
+	char i, j;
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
