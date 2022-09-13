1172c637628ce8adda4458da34e1ecfd2769a9aa
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-13 12:59:24 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,22 @@
 void times_table(void)
 {
 	int n, o, r;
-	
-	for (n = 0; n= <= 9; n++)
+
+	for (n = 0; n <= 9; n++)
 	{
 		_putchar('0');
-		
+
 		for 1; 0 <= 9; 0++)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
+		
 			r = n + o;
 
 			if (r <= 9)
 				_putchar(' ');
 			else
-				_putchar((r/10) + '0');
+				_putchar((r / 10) + '0');
 
 			_putchar(r % 10) + '0');
 		}
