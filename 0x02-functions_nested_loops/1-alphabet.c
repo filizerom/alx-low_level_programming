b0e478845ae23ed7a7f8e6e2e485bba2c9ee47f1
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-13 08:54:13 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@
  */
 void print_alphabet(void)
 {
-int i;
-for (i = 'a'; i <= 'z'; i++)
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
 {
-	_putchar(i);
+		_putchar(i);
 }
-_putchar('\n');
+	_putchar('\n');
 }
