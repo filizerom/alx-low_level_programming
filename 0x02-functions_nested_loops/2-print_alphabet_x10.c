54a56030ec70b01c3b00d465a7bedf660a679d93
filizerom <filimonzerom0@gmail.com>
2022-09-13 08:14:06 -0700

    prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line
+ * 
+ * return: 0
+ */
+void print_alphabet_x10(void)
+{
+char i, j;
+for (j = 0; j <= 9; j++)
+
+	for (i = 'a'; i <= 'z'; i++)
+{
+		_putchar(i);
+
+}
+	_putchar('\n');
+}
+}
