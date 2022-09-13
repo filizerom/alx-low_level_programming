787140d93c378936be8583247bee0553764a3e2e
filizerom <filimonzerom0@gmail.com>
2022-09-13 06:09:44 -0700

    try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,11 @@
 #include "main.h"
 /**
- * main - to write a program that prints _putchar.
- * Description: use main function.
+ * main - to print putchart
  * return 0
  */
+
 int main(void)
-/**
- * to use as main 
- *
- */
 {
-char *lk = "_putchar";
-while (*lk)
-{
-	_putchar(*lk);
-	lk++;
-}
-_putchar('\n');
+_putchar(' _putchar'
 return (0);
+}
