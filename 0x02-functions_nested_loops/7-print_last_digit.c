c410f939bc9384523e310c55867761bf61e312f7
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-13 12:09:45 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_last_digit - last digit number
+ * int print_last_digit
  * @n: n for arguement
  * Return: n
  */
 int print_last_digit(int)
 {
 	int m;
+
 	if (n < 0)
-n = -n;
+		n = -n;
 	x = n % 10;
 	_putchar(X + '0');
 	return (n % 10);
