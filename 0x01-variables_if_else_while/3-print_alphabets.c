c5d48cd4b246897ca5218da8dcec6e2ab9bf3ff6
filizerom <filimonzerom0@gmail.com>
2022-09-12 20:18:46 -0700

    test

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+/**
+ * main - print upper and lower case
+ * Description: use main function
+ * Return: 0
+ */
+int main(void)
+{
+char alp;
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+putchar(alp);
+}
+for (alp = 'A'; alp <= 'Z'; alp++)
+{
+	putchar(alp);
+}
+putchar('\n');
+return (0);
