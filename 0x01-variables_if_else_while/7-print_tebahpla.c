6beee4e78854526fe2d8cee8881e9b3adb869c28
filizerom <filimonzerom0@gmail.com>
2022-09-12 22:34:03 -0700

    print lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print lower case in reverse
+ * Description: use main function and char
+ * return 0
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
