83f8375ace47087c464fb9e06309dbbfcea3aba1
filizerom <filimonzerom0@gmail.com>
2022-09-13 11:21:44 -0700

    9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table-print 9 times
+ *
+ */
+void times_table(void)
+{
+	int n, o, r;
+	
+	for (n = 0; n= <= 9; n++)
+	{
+		_putchar('0');
+		
+		fo 1; 0 <= 9; 0++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			
+			r = n + o;
+
+			if (r <= 9)
+				_putchar(' ');
+			else
+				_putchar((r/10) + '0');
+
+			}
+			_putchar(r % 10) + '0');
+}
+			_putchar('\n');
+}
+}
