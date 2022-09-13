df6d2791bf28ac756f34e8d1e9bda95b13d82d1d
filizerom <filimonzerom0@gmail.com>
2022-09-13 07:43:27 -0700

    print all alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print lowercase alphabets
+ * Description: use main function
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 'a'; i = <= 'z'; i++)
+{	
+		_putchar(i);
+}
+	_putchar('\n');
+}
