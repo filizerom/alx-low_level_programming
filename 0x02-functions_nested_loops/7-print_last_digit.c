2b2301afb2a1839180bb305f3e793ffe888a0d5f
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-14 00:53:25 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,17 @@
 /**
  * print_last_digit - last digit of number
  * @n: a number
- * 
+ *
  * Return: last digit
  */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	
+
 	if (last_digit < 0)
 		last_digit *= -1;
-	
+
 	_putchar(last_digit + '0');
-	
-	return(last_digit);
+
+	return (last_digit);
 }
