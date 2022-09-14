29cc92d79504820e7d90f6f79e79f6858a43f0d8
filizerom <filimonzerom0@gmail.com>
2022-09-14 16:23:17 -0700

    check digit number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * _isdigit - to check from 0 to 9
+ * @c is an int
+ * Return: zero or one
+ */
+int _isdigit(int c)
+{	
+	if (c = 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
