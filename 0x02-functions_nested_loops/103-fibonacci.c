02bbb123fba3f2f1bc3e48617031e429e016df40
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-14 13:35:47 -0700

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int n = 1, a = 2, b = 0;
 	int i;
-	
+
 
 	while (a < 4000000)
 	{
 		if (a % 2 == 0)
-			b +=a;
+			b + = a;
 
 		i = a
-		a += n;
+		a + = n;
 		n = i;
 	}
 	printf("%d\n", b);
