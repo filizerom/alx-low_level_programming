4b1c2416ee3d273143c76ef982182bd48503f25e
filizerom <filimonzerom0@gmail.com>
2022-09-14 09:54:25 -0700

    sum multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * add - print the sum of all multiple 3 and 5
+ * return 0
+ */
+int main(void)
+{
+	int sum1 = 0, int sum2 = 0;
+	int i;
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0)
+
+		{
+			sum1 += i;
+		}
+		if (i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum1 + sum2);
+	return 0;
