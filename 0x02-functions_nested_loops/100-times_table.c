c29f541aea2143812ed58886907979af184de5e8
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-14 05:27:15 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,39 +2,37 @@
 /**
  * void print_times_table - print n times
  * @n - is a number
- * 
+ *
  */
 void print_times_table(int n)
 {
 	int x, y, z;
+	
 	if (n <= 15)
 	{
 		for (x = 0; x <= n; x++)
-		{	
+		{
 			_putchar('0');
-				
+	
 			for (y = 1; y <= n; y++)
 			{
 				_putchar(',');
       				_putchar(' ');
-					  
+  
 				z = x * y;
-					 
+
 				if (z <= 9)
 					_putchar(' ');
-				
+
 				else
 					_putchar((z / 10) + '0');
-			
-			
+
 				_putchar((z % 10) + '0');
-			    
-                         }
+
+				}
 			_putchar('\n');
 		}
 
 	}
-	
-		
-	
+
 }
