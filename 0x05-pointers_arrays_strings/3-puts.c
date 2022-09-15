3ce2710d5096766d176b063ac770d97f104490c2
filizerom <filimonzerom0@gmail.com>
2022-09-15 16:47:02 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - to print a string of stdout
+ * @*str: a string 
+ *return zero
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while (*(str + i) != '\0')
+
+{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10)
+}
