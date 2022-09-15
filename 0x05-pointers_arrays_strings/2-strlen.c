42f852a7715a289440455a8f50da62eca0396648
filizerom <filimonzerom0@gmail.com>
2022-09-15 15:26:56 -0700

    return the length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: a character
+ * return - 0
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*(s + length) != '\0')
+		length++;
+	return (length);
+}
