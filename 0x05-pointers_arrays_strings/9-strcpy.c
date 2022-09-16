fb1592dfe460d364e1253787a43518ed10f20b56
filizerom <filimonzerom0@gmail.com>
2022-09-15 20:07:00 -0700

    copy the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * -strcpy - copy the string pointed to by src
+ * @dest: a character
+ * @src: a character
+ * return - 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a++] = '\0';
+	return (dest);
+}
