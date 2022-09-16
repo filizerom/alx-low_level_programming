de82881a5b083d2774f09fe9d0a9044a780b1c74
filizerom <filimonzerom0@gmail.com>
2022-09-16 16:54:59 -0700

    task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - to add one line to this code
+* make sure you are allowed only one code
+*/
+int main(void)
+{
+	int n;
+	int a[s];
+	int *p;
+	
+	a[2] = 1024;
+	p = &n;
+
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}
