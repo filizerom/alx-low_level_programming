0b8f50cceec1c6d4073de40f128ca817fa9e8c34
filizerom <filimonzerom0@gmail.com>
2022-09-16 11:08:17 -0700

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,10 +7,10 @@
 */
 char *leet(char *c)
 {
-	int i =0, j, k = 5;
+	int i = 0, j, k = 5;
 	char a[5] = {'A', 'E', 'O', 'T', 'L'};
 	char b[5] = {'4', '3', '0', '7', '1'};
-	
+
 	while (c[i])
 	{
 		j = 0;
