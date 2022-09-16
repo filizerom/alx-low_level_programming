5f5e4575e67645c03bdd4cb51ddccde58a75395a
filizerom <filimonzerom0@gmail.com>
2022-09-16 08:26:46 -0700

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* reverse_array
+* @a: a character
+* @n: an integer
+* return: zero
+*/
+void reverse_array(int *a, int n);
+{
+	int *p, b, aux, c;
+	p = a;
+	for (a = 0; a < n; a++)
+		p++;
+	for (c = 0; c < n / 2; c++)
+	{
+		aux = a[c];
+		a[c] = *p;
+		*p = aux;
+		p--;
+	}
+}
