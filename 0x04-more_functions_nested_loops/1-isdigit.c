9325edb15ea81fe7291a532fbcd6b1f391b50d2f
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-16 00:23:45 -0700

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * _isdigit - to check from 0 to 9
  * @c is an int
@@ -6,7 +6,7 @@ include "main.h"
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
