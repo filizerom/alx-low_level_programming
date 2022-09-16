6c07a54fd26cfc69c21bf15b81c5bb7426f0f2c5
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-16 03:32:37 -0700

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,21 +5,21 @@
  */
 int main(void)
 {
-  long num = 612852475143;
-  int i;
-  while (i++ < num / 2)
-  {
-    if (num % 2 == 0)
-    {
-      num /= 2;
-      continue;
-    }
-    for (i = 3; i < num / 2; i += 2)
-    {
-      if (num % i == 0)
-        num /= i;
-    }
-  }
-  printf("%ld\n", num);
-  return (0);
+long num = 612852475143;
+int i;
+while (i++ < num / 2)
+{
+if (num % 2 == 0)
+{
+num /= 2;
+continue;
+}
+for (i = 3; i < num / 2; i += 2)
+{
+if (num % i == 0)
+num /= i;
+}
+}
+printf("%ld\n", num);
+return (0);
 }
