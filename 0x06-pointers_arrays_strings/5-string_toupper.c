cad6a9ad1ddf3b188c8377a7a5cff3548a55ba8f
filizerom <filimonzerom0@gmail.com>
2022-09-16 08:44:19 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* string_toupper - change all lowercase to uppercase
+* @n: a character
+* Return: n
+*/
+char *string_toupper(char *n)
+{
+	int a = 0;
+
+	while (n[a])
+	{
+		if (n[a]) >= 'a' && n[a] <= 'z')
+			n[a] = n[a] - 32;
+		a++;
+	}
+	return (n);
+}
