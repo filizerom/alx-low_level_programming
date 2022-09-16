4938d1ac59eca42dbfb057bb2f79bb86be9051ca
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-16 03:56:43 -0700

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,10 +13,9 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-	if (num > 9)
+	if (num > 0)
 	{
 		print_number(num / 10);
-	 _putchar((num % 10) + '0');
 	}
-
+	 _putchar(num % 10 + '0');
 }
