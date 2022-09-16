bce438fd5791357784b442ce4b896c21f267dbcf
filizerom <filimonzerom0@gmail.com>
2022-09-16 15:26:04 -0700

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,11 @@ char *cap_string(char *c)
 {
 	int i = 0;
 
-	while (c[++i])
+	while (c[i])
 	{
 		while (!(c[i] >= 'a' && c[i] <= 'z'))
 			i++;
-		if (c[i - 1] == '' ||
+		if (c[i - 1] == ' ' ||
 			c[i - 1] == '\t' ||
 			c[i - 1] == '\n' ||
 			c[i - 1] == ',' ||
@@ -26,8 +26,11 @@ char *cap_string(char *c)
 			c[i - 1] == '(' ||
 			c[i - 1] == ')' ||
 			c[i - 1] == '{' ||
-			c[i - 1] == '}')
-		str[i] -= 32;
+			c[i - 1] == '}' ||
+			i == 0)
+				c[i] -= 32;
+
+		i++;
 
 	}
 	return (c);
