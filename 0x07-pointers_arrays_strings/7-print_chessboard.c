93b449824733a77dcd4ead0118e242384e4098de
filizerom <filimonzerom0@gmail.com>
2022-09-17 07:20:27 -0700

    task7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_chessboard - a function that prints the chessboard
+* @a: a character
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
