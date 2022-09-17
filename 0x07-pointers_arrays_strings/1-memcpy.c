c8083dbddeccb4d6439d07bfaabe316131c7a0fa
filizerom <filimonzerom0@gmail.com>
2022-09-17 06:01:39 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_memcpy -  function copies n bytes from memory 
+* @n: unsigned integer
+* @src: a character
+* @dest: a character
+* return - dest wiil be
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+	
+	for ( i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+return (dest);
+}
+
