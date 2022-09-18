8ccf9635bd382a413da43d92637cb4d122d99949
filizerom <filimonzerom0@gmail.com>
2022-09-17 17:31:57 -0700

    task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* is_palindrome - function that returns 1 if a string is a palindrome and 0 if not
+* Return: zero
+*/
+int is_palindrome(char *s)
+{
+	int i;
+	i = palindm(s, 0);
+	return (palindm1(s, i)
+}
