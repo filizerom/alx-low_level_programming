f3656125e628daf62fdda50e69c85e33bdafa7be
filizerom <filimonzerom0@gmail.com>
2022-09-17 21:15:34 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - a function
+* @argc: an arguement
+* @@argv: an arguement
+* Return: zero(0)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
