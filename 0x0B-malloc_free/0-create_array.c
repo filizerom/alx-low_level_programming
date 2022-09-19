dd4130aa792337cefe5df0bd064eb48a7b761be2
filizerom <filimonzerom0@gmail.com>
2022-09-18 19:30:11 -0700

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* create_array - creates an array of chars, and initializes it with a specific char
+* create_array - creates an array of chars, and initializes
 * @size: an integer
 * @c: a character
 * Return: NULL or zero
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* strtow - a function that splits a string into words
+* @str: a character
+* Return: NULL
+*/
+char **strtow(char *str)
+{
+	char **s;
