1dbec2d9092cacb5843552941d5d82fc751e5290
filizerom <filimonzerom0@gmail.com>
2022-09-19 16:49:55 -0700

    fix

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* array_range - function that reallocates a memory
+* block using malloc and free
+* @min: an integer
+* @max: an integer
+* Return: the pointer
+*/
+int *array_range(int min, int max)
+{
+	int *arr, i, size;
+
+	if (min > max)
+		return (NULL)
+	size = max - min + 1;
+
+	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
+
+	return (arr);
+}
