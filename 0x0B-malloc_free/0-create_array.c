390ced469dd418301b38e5c21d077d9889779c08
filizerom <filimonzerom0@gmail.com>
2022-09-18 17:17:05 -0700

    task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* create_array - creates an array of chars, and initializes it with a specific char
+* @size: an integer
+* @c: a character
+* Return: NULL or zero
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *j;
+
+	if (size == 0)
+		return (NULL);
+
+	j = malloc(size * sizeof(char));
+
+	if (j == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		j[i] = c;
+	}
+	return (j);
+
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_strdup - returns a pointer to a newly allocated space in memory
+* @str - a character
+* Return: NULL
+*/
+char *_strdup(char *str)
+{
+	int i = 0, j = 1;
+	char *k;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		j++:
+	}
+	k = malloc((sizeof(char) * j) + 1);
+
+	k = malloc(sizeof(char) * j) + 1);
+
+	if (k == NULL)
+		return (NULL);
+
+	while (i < j)
+	{
+		k{i] = str[i];
+		i++;
+	}
+	k[i] = '\0';
+	return (k);
+}
