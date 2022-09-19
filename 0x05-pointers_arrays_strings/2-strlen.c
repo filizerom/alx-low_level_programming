f034348970a40daa350ff5ee5eaa387fd1773ef6
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-19 11:01:54 -0700

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,12 @@
 /**
  * _strlen - return the length of a string
  * @s: a character
- * return - 0
+ * Return: nothing or zero 
  */
 int _strlen(char *s)
 {
 	int length = 0;
-	
+
 	while (*(s + length) != '\0')
 		length++;
 	return (length);
