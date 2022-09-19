17e34ec2efd251b94c6ea522c296ce42654a458e
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-19 06:21:38 -0700

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts - to print a string of stdout
- * @*str: a string 
+ * @*str: a string
  *return zero
  */
 void _puts(char *str)
 {
 	int i = 0;
-	while (*(str + i) != '\0')
 
-{
+	while (*(str + i) != '\0')
+	{
 		putchar(*(str + i));
 		i++;
 	}
-	putchar(10)
+	putchar(10);
 }
