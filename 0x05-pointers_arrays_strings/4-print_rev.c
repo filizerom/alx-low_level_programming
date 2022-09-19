610da9256024f19f301436fbb44ada3ea28e2fac
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-19 08:39:16 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
 
 /**
  * print_rev - to print a string, in reverse followed by a new line.
- * @s - a character
+ * @*s: - a character
  * return - 0
  */
 void print_rev(char *s)
@@ -15,5 +15,3 @@ void print_rev(char *s)
 		putchar(*(s + length));
 	putchar('\n');
 }
-
-	
