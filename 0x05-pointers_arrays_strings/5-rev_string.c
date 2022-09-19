5cd8aefb0984629ad1451653cf0a9e87ae73b6d5
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-19 11:46:45 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,24 +6,17 @@
  */
 void rev_string(char *s)
 {
-	int a, b, c;
+	int a = 0, b = 0;
+	char tmp;
 
-	char *d, aux;
+	while (s[b++])
 
-	d = s;
-	while (s[b] != '\0')
-	{
 		b++;
-	}
-	for (c = 1; c < b; c++)
+
+	for (b = a - 1; b >= a / 2; b--)
 	{
-		d++;
-	}
-	for (a = 0; a < (b / 2); a++)
-	{
-		aux = s[a];
-		s[a] = *d;
-		*d = aux;
-		d--;
+		tmp = s[b];
+		s[b] = s[a - b -1];
+		s[a -b - 1] =tmp;
 	}
 }
