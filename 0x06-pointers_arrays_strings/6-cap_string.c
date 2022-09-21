79a1a38a265a9f1f803377abd429a3472fecf74a
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-21 08:06:49 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,34 +4,32 @@
 /**
 * cap_string - function that capitalizes all words of a string.
 * @c: a character
-* return: zero
+* return: will be c
 */
 char *cap_string(char *c);
 {
 	int i = 0;
-	
+
 	while (c[++i])
 	{
 		while (!(c[i] >= 'a' && c[i] <= 'z'))
 			i++;
 		if (c[i - 1] == ' ' ||
-			c[i-1] == '\t' ||
-			c[i-1] == '\n' ||
-			c[i-1] == ',' ||
-			c[i-1] == ';' ||
-			c[i-1] == '.' ||
-			c[i-1] == '!' ||
-			c[i-1] == '?' ||
-			c[i-1] == '"' ||
-			c[i-1] == '(' ||
-			c[i-1] == ')' ||
-			c[i-1] == '{' ||
-			c[i-1] == '}')
- 
+			c[i - 1] == '\t' ||
+			c[i - 1] == '\n' ||
+			c[i - 1] == ',' ||
+			c[i - 1] == ';' ||
+			c[i - 1] == '.' ||
+			c[i - 1] == '!' ||
+			c[i - 1] == '?' ||
+			c[i - 1] == '"' ||
+			c[i - 1] == '(' ||
+			c[i - 1] == ')' ||
+			c[i - 1] == '{' ||
+			c[i - 1] == '}')
+
 		str[i] -= 32;
 
 	}
 	return (c);
 }
-				
-
