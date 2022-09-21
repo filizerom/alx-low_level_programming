63c3a20240c7bd3c69b0f48548a072d423b9fb7a
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-21 01:19:47 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,12 +5,12 @@
  * _strcat - appends the src string to the dest string
  * @dest: a character
  * @src: a character
- * return: character dest
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int dis, i;
-	
+
 	while (dest[dis])
 	dis++;
 	for (i = 0; src[i] != '\0'; i++)
