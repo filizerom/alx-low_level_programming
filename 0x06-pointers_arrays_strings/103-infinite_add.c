b87fa9d323699c062cb22df1026635623ad9ba08
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-23 04:43:07 -0700

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+char *infinite_add(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
 * infinite_add - a function that adds two numbers
 * @n1: a character
@@ -7,6 +10,34 @@
 * @size_r: an integer
 * Return: zero
 */
+char *infinite_add(char *n1, char *n2, char *r, int r_index)
+{
+	int num, ten = 0;
+	
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += ten;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + ten;
+		*(r + r_index) = (num % 10) + '0';
+		ten = num / 10;
+	}
+	if (ten && r_index >= 0)
+	{
+		*(r + r_index) = (ten % 10) + '0';
+		return (r + r_index);
+	}
+	else if (ten && r_index < 0)
+		return (0);
+
+	return (r + r_index +1);
+
+		
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, n3 = 0, n4 = 0;
@@ -22,5 +53,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	n2 += n4 - 1;
 	*(r + size_r) = '\0';
 
-	return (0);
+	return (add_strings(n1, n2, r, --size_r));
 }
