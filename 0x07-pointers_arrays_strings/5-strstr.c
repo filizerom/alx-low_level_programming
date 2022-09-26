44c79f3454ed1c3b3ce0b76a6094c836a9f632c4
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-26 13:31:01 -0700

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,26 +9,24 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-
-	if (needle == 0)
-		return (haystack);
-	while (haystack)
+	int i = 0, j = 0;
+	
+	while (haystack[i])
 	{
-		i = 0;
-
-		if (haystack[i] == needle[i])
+		while (needle[j])
 		{
-			do {
-				if (needle[i + 1] == '\0')
-					return (haystack);
-
-				i++;
-
-
-			} while (haystack[i] == needle[i]);
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+			j++;
 		}
-		haystack++;
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+		i++;
 	}
+	
 	return ('\0')
 }
