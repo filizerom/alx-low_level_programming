0cd6bc6daea2e70199478745d26e0cf836d0985c
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-28 09:38:58 -0700

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,19 @@
 #include "main.h"
+/**
+* is_prime_number1 - evaluate from 1 to n
+* @a: an integer
+* @b: an integer
+* Return: zero
+*/
+int is_prime_number1(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+
+}
 
 /**
 * is_prime_number - function that returns 1 if prime number
