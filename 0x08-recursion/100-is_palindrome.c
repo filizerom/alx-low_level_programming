abaf0ebd8c6da4af351cc797b3ea7aeb92a4c78b
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-28 10:51:13 -0700

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,38 @@
 #include "main.h"
 /**
+* palindm1 - obtains length of i
+* @i: a character
+* @j: an intger
+* Return: zero(0)
+*/
+int palindm1(char *i, int j)
+{
+	if (*i == 0)
+		return (j - 1);
+	return (palindm1(i + 1, j + 1));
+}
+/**
+* palindm2 - compares string vs string reverse
+* @i: a character
+* @j: an integer
+* Return: 0
+*/
+int palindm2(char *i, int j)
+{
+	if (*i != *(i + j))
+		return (0);
+	else if (*i == 0)
+		return (1);
+	return (palindm2(i + 1, j - 2));
+}
+
+/**
 * is_palindrome - function that returns 1 if a string is a palindrome and 0 if not
 * Return: zero
 */
 int is_palindrome(char *s)
 {
-	int i;
-	i = palindm(s, 0);
-	return (palindm1(s, i)
+	int j;
+	j = palindm(s, 0);
+	return (palindm1(s, j)
 }
