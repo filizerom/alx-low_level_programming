25070c5c4f2978c72f69094a632aed53cc35d5b0
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-30 12:08:44 -0700

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,12 @@
 /**
 * main - a function
 * @argc: an arguement
-* @@argv: an arguement
+* @argv: an arguement
 * Return: zero(0)
 */
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
-	
+
 	return (0);
 }
