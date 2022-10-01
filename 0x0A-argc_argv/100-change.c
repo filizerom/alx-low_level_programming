42f3dd08d0f29a67b9cc08bd2cc37db53a73db0b
filizerom <111068083+filizerom@users.noreply.github.com>
2022-09-30 19:53:17 -0700

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,12 +7,37 @@
 */
 int main(int argc, char *argv[])
 {
-	int a;
+	int a, b = 0;
 
-	for (a = 0; a < argc; a++)
+	if (argc != 2)
+	    printf("Error\n");
+	    return (1);
+
+	a = atoi(argv[1]);
+
+	while (a > 0)
 	{
-		printf("%s\n", argv[a];
+		b++;
+		if ((a - 25) >= 0)
+			a -=25;
+			continue;
+	
+		if ((a - 10) >= 0)
+			a -=10;
+			continue;
 
+		if ((a - 5) >= 0)
+			a -=5;
+			continue;
+
+		if ((a -2) >=0)
+			a -=2;
+			continue;
+		a--;
 	}
+
+	printf("%d\n", b);
+
 	return (0);
 }
+
