8227ea2ad612b9536d3f6dcce3f42f154f5ee34e
filizerom <filimonzerom0@gmail.com>
2022-10-10 14:21:25 -0700

    task5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/*
+* free_dog -  function that frees dogs.
+* @d: to free the dog
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
+
