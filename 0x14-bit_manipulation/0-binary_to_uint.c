59c694be98b5108604d3af20adc2510a4c5bc219
filizerom <filimonzerom0@gmail.com>
2022-10-12 08:33:58 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,8 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == '\0')
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
+	for (len = 0; b[len]; len++)
 
 	for (len -= 1; len >= 0; len--)
 	{
