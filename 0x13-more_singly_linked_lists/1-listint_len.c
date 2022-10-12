364004e1c36e93bdc96e9122848fa589e67f1a55
filizerom <filimonzerom0@gmail.com>
2022-10-12 04:25:28 -0700

    fix

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,10 @@
+nclude "lists.h"
+#include <stdio.h>
+/**
+* listint_len - Returns the number of elements
+*   in a linked listint_t list
+*@h: A pointer to the head of the listint_t list
+*Return: The number of elements in the listint_t list.
+*/
+
+
